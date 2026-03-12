c43d234e7908172af872ee99f0af319e76fff13b
Harold Saenz <joshu4nick19@gmail.com>
2026-03-12 02:06:47 -0400

    Styling changed

--- Frontend_Web/LesChic/src/components/Signup.tsx
+++ Frontend_Web/LesChic/src/components/Signup.tsx
@@ -2,9 +2,9 @@
 export const Signup = () =>{
     return(
         <form className="px-10 py-8 flex flex-col gap-3 animate-in fade-in duration-500">
-            <div className="flex gap-3">
-                <input type="text" placeholder="First" required className="w-1/2 bg-white/10 border border-accent/20 rounded-xl px-5 py-3 text-white/80 text-sm placeholder:text-white/50 outline-none focus:border-white/30 tracking-wider transition" />
-                <input type="text" placeholder="Last" required className="w-1/2 bg-white/10 border border-accent/20 rounded-xl px-5 py-3 text-white/80 text-sm placeholder:text-white/50 outline-none focus:border-white/30 tracking-wider transition" />
+            <div className="flex flex-col gap-3">
+                <input type="text" placeholder="First" required className="w-full bg-white/10 border border-accent/20 rounded-xl px-5 py-3 text-white/80 text-sm placeholder:text-white/50 outline-none focus:border-white/30 tracking-wider transition" />
+                <input type="text" placeholder="Last" required className="w-full bg-white/10 border border-accent/20 rounded-xl px-5 py-3 text-white/80 text-sm placeholder:text-white/50 outline-none focus:border-white/30 tracking-wider transition" />
             </div>
             <input type="email" placeholder="Email" required className="w-full bg-white/10 border border-accent/20 rounded-xl px-5 py-3 text-white/80 text-sm placeholder:text-white/50 outline-none focus:border-white/30 tracking-wider transition" />
             <input type="password" placeholder="Password" required className="w-full bg-white/10 border border-accent/20 rounded-xl px-5 py-3 text-white/80 text-sm placeholder:text-white/50 outline-none focus:border-white/30 tracking-wider transition" />
