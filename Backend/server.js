5e7d0d944bdee36c51472c26d907da20f462ce12
Nicolas Albornoz <nico.albornoz.s@gmail.com>
2026-03-28 20:40:09 -0400

    cors

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,5 @@
-import express from 'express'
+import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 
 import { dbConnection } from './database/config.js';
@@ -10,6 +11,9 @@ const app = express();
 const port = process.env.PORT;
 
 dbConnection();
+
+//cors
+app.use(cors())
 
 //body Parse
 app.use( express.json() )
