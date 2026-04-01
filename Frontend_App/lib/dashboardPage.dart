e1fffbeeab3448db38c46626f73e3fd46785c10a
Rae <60368869+x-rae@users.noreply.github.com>
2026-04-01 19:42:15 -0400

    add tabs to dashboard
    they don't do anything yet

--- Frontend_App/lib/dashboardPage.dart
+++ Frontend_App/lib/dashboardPage.dart
@@ -6,6 +6,33 @@ class DashboardPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        destinations: const<Widget>[
+          //overview
+          NavigationDestination(
+            icon: Icon(Icons.dashboard), 
+            label: 'Home'
+          ),
+
+          //my closet
+          NavigationDestination(
+            icon: Icon(Icons.checkroom), //TODO: replace with shirt icon
+            label: 'My Closet'
+          ),
+
+          //lookbooks
+          NavigationDestination(
+            icon: Icon(Icons.list), //TODO: replace with shirt icon
+            label: 'Lookbooks'
+          ),
+
+          //planner
+          NavigationDestination(
+            icon: Icon(Icons.calendar_month), //TODO: replace with shirt icon
+            label: 'Planner'
+          ),
+        ],
+      ),
       appBar: AppBar(
         title: const Text('Dashboard'),
       ),
