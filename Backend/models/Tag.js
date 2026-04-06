f4830de198d2cf287d2bea5bf43812b410ffa112
Nicolas Albornoz <nico.albornoz.s@gmail.com>
2026-04-06 01:17:36 -0400

    fix bug: Not sending id on Tags

--- Backend/models/Tag.js
+++ Backend/models/Tag.js
@@ -14,11 +14,13 @@ const TagSchema = new Schema({
     }
 });
 
-TagSchema.method('toJSON', function(){
-    const {__v, _id, ...object} = this.toObject()
-    object.id = _id
-
-    return object
+TagSchema.set('toJSON', {
+    transform: function (doc, ret) {
+        delete ret.__v;
+        ret.id = ret._id;
+        delete ret._id;
+        return ret;
+    }
 });
 
 TagSchema.set('toObject', {
