0eb1766cc86ce7b812830951f92ac5676c6bfed3
Harold Saenz <joshu4nick19@gmail.com>
2026-04-08 23:40:46 -0400

    buildpath updated for PROD

--- Frontend_Web/LesChic/src/utils/buildPath.ts
+++ Frontend_Web/LesChic/src/utils/buildPath.ts
@@ -2,14 +2,12 @@ export const buildPath = (route: string): string => {
     // const app_name = 'leschic-app';
     
     // Commented out for now as we are only working locally
-    /*
+    
     if (import.meta.env.PROD) {
-        return `https://${app_name}.herokuapp.com/${route}`;
+        return `http://www.ec-albo.xyz:5000/${route}`;
     } else {
+        // the fall back
         return `http://localhost:5000/${route}`;
     }
-    */
 
-    // Direct local return for current development
-    return `http://localhost:5000/${route}`;
 };
