235806e76eebac94bb49abd6dd09d10df62cbda7
Rae <60368869+x-rae@users.noreply.github.com>
2026-04-11 15:00:15 -0400

    move NavigationBar into own class
    * makes code easier to read
    * easier to comment out, since it will likely not be in the final product

--- Frontend_App/lib/dashboardPage.dart
+++ Frontend_App/lib/dashboardPage.dart
@@ -6,33 +6,7 @@ class DashboardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        destinations: const <Widget>[
-          //overview
-          NavigationDestination(
-            icon: Icon(Icons.dashboard), 
-            label: 'Home',
-          ),
-
-          //my closet
-          NavigationDestination(
-            icon: Icon(Icons.checkroom), //TODO: replace with a better icon
-            label: 'My Closet',
-          ),
-
-          //lookbooks
-          NavigationDestination(
-            icon: Icon(Icons.list), //TODO: replace with a better icon
-            label: 'Lookbooks',
-          ),
-
-          //planner
-          NavigationDestination(
-            icon: Icon(Icons.calendar_month), //TODO: replace with a better icon
-            label: 'Planner',
-          ),
-        ],
-      ),
+      bottomNavigationBar: DashboardNavBar(),
       appBar: AppBar(
         title: const Text('Dashboard'),
       ),
@@ -42,6 +16,41 @@ class DashboardPage extends StatelessWidget {
           child: const Text('Log out'),
         ),
       ),
+    );
+  }
+}
+
+class DashboardNavBar extends StatelessWidget {
+  const DashboardNavBar ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationBar(
+      destinations: const <Widget>[
+        //overview
+        NavigationDestination(
+          icon: Icon(Icons.dashboard), 
+          label: 'Home',
+        ),
+
+        //my closet
+        NavigationDestination(
+          icon: Icon(Icons.checkroom), //TODO: replace with a better icon
+          label: 'My Closet',
+        ),
+
+        // lookbooks
+        NavigationDestination(
+          icon: Icon(Icons.list), //TODO: replace with a better icon
+          label: 'Lookbooks',
+        ),
+
+        //planner
+        NavigationDestination(
+          icon: Icon(Icons.calendar_month), //TODO: replace with a better icon
+          label: 'Planner',
+        ),
+      ],
     );
   }
 }
