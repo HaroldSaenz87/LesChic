7dfda9ae095fd67ef3d8363137962d281f40bacc
Rae <60368869+x-rae@users.noreply.github.com>
2026-04-14 10:45:42 -0400

    change theme seed color
    this one is also taken from the palette, but seems to have a better contrast

--- Frontend_App/lib/main.dart
+++ Frontend_App/lib/main.dart
@@ -21,7 +21,7 @@ class App extends StatelessWidget {
       //set the theme TODO: add color palette
       theme: ThemeData(
         fontFamily: FONT_NAME,
-        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromRGBO(242, 166, 171, 1)),
+        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromRGBO(193, 147, 138, 1)),
         inputDecorationTheme: InputDecorationTheme(
           border: OutlineInputBorder(),
         ),
